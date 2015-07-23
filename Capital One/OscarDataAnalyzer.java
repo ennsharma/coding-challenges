18a44163e95c626233615aa80d3f1f49d65c3928
Nikhil <ennsharma@berkeley.edu>
2015-07-23 14:50:13 -0700

    Added Capital One's Oscars Data Analysis Challenge

--- Capital One/OscarDataAnalyzer.java
+++ Capital One/OscarDataAnalyzer.java
@@ -5,6 +5,7 @@ import java.lang.*;
 import java.util.Collections;
 
 public class OscarDataAnalyzer {
+	/* Main method */
 	public static void main(String[] args) {
 		long startTime = System.currentTimeMillis();
 		
